123f7f97532a71d2786831136c99160322bea87b
tausif4802 <67308400+tausif4802@users.noreply.github.com>
2022-10-20 02:20:14 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.set('trust proxy', true);
+app.get('/', (req, res) => {
+  console.log(req);
+  console.log(req.ip);
+  res.send('Server is running');
+});
+
+const PORT = 3000;
+
+server.listen(PORT, console.log(`Server started on port: ${PORT}`));
